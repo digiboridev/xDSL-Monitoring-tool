398aaf1caf1473d42b5c39bc0ae59e004fd29e5f
digiborimusik <digiborimusik@gmail.com>
2023-10-20 20:10:21 +0300

    fixes: foreground text, back to main activity

--- android/app/src/main/kotlin/com/digiboridev/xdslmt/ForegroundService.kt
+++ android/app/src/main/kotlin/com/digiboridev/xdslmt/ForegroundService.kt
@@ -1,6 +1,8 @@
 package com.digiboridev.xdslmt
+
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
@@ -31,23 +33,43 @@ class ForegroundService : Service() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         println("FOREGROUND SERVICE STARTED")
         try {
+            // Create the notification channel
             this.createNotificationChannel()
+
+            // Create intent for return to MainActivity
+            val pendingIntent = this.createPendingIntent()
+
+            // Create the notification
             val notification = NotificationCompat.Builder(this, notificationChannelId)
                 .setOngoing(true)
+                .setSmallIcon(R.mipmap.ic_launcher_adaptive_fore)
+                .setContentTitle("xDSL monitoring tool")
+                .setContentText("Sampling service is running")
+                .setContentIntent(pendingIntent)
                 .build()
+
+            // To foreground
             startForeground(999, notification)
         } catch (e: Exception) {
             println("FOREGROUND SERVICE START EXCEPTION: $e")
         }
-
         return START_STICKY
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun createNotificationChannel() {
-        val channel = NotificationChannel(notificationChannelId, "Foreground service", 3)
+        val channel = NotificationChannel(
+            notificationChannelId,
+            "Foreground service",
+            NotificationManager.IMPORTANCE_DEFAULT,
+        )
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.createNotificationChannel(channel)
+    }
+
+    private fun createPendingIntent(): PendingIntent {
+        val mainIntent = Intent(this, MainActivity::class.java)
+        return PendingIntent.getActivity(this, 0, mainIntent, PendingIntent.FLAG_MUTABLE)
     }
 }

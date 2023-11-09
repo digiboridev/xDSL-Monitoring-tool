6047fe1a7c1cd13acd83ffa696e3af2a884fe63c
digiborimusik <digiborimusik@gmail.com>
2023-11-09 21:20:11 +0200

    network unit: bcm impl tests

---
+++ test/data/net_unit_clients/broadcom_adslcmd_sh_impl_test.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: avoid_print
+import 'dart:io';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:xdslmt/data/models/line_stats.dart';
+import 'package:xdslmt/data/net_unit_clients/broadcom_adslcmd_sh_impl.dart';
+import 'package:xdslmt/data/net_unit_clients/net_unit_client.dart';
+import '../../telnet_emulator/telnet_emulator.dart';
+
+void main() {
+  test('broadcom adslcmd sh impl  > success flow', () async {
+    final closeEmu = await startEmulator(
+      shellSkip: false,
+      command2Stats: (
+        cmd: 'adslcmd info --show',
+        file: File('test/telnet_emulator/stats_examples/bcmstats_adsl.txt'),
+      ),
+    );
+
+    final NetUnitClient client = BroadcomAdslcmdShClientImpl(
+      unitIp: '0.0.0.0',
+      snapshotId: 'test',
+      login: 'admin',
+      password: 'admin',
+    );
+
+    final stats = await client.fetchStats();
+    expect(stats.status, SampleStatus.connectionUp);
+    print(stats);
+    await closeEmu();
+  });
+}

1b7c23a44c58997d18573936c775573cf1e7357a
digiborimusik <digiborimusik@gmail.com>
2023-11-14 20:39:25 +0200

    logger: config sentry

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,45 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:logging/logging.dart';
 import 'package:provider/provider.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:xdslmt/core/sl.dart';
 import 'package:xdslmt/data/repositories/stats_repo.dart';
 import 'package:xdslmt/data/repositories/settings_repo.dart';
-import 'package:xdslmt/data/services/stats_sampling_service.dart';
+import 'package:xdslmt/data/services/stats_sampling_service.dart' hide log;
 import 'package:xdslmt/screens/screens_wrapper.dart';
 
-void main() {
+Future<void> main() async {
+  // Logger configuration
   Logger.root.level = Level.ALL;
   Logger.root.onRecord.listen((record) {
     final logerName = record.loggerName.isEmpty ? 'ROOT' : record.loggerName;
     final level = record.level.name;
     final time = record.time;
 
-    debugPrint('$time $level $logerName:\n${record.message}');
+    // Setup local log
+    debugPrint('$time $level $logerName:');
+    debugPrint(record.message);
+    if (record.error != null) debugPrint('${record.error}');
+    if (record.stackTrace != null) debugPrint('${record.stackTrace}');
+
+    // Setup remote log
+    if (record.error != null) Sentry.captureException(record.error, stackTrace: record.stackTrace);
+    // Sentry will captures rest of logs as breadscrumps automatically, because of enablePrintBreadcrumbs
   });
 
-  runApp(const App());
+  // Entry point
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = const String.fromEnvironment('sentryKey');
+      options.tracesSampleRate = 1.0;
+      options.enablePrintBreadcrumbs = true;
+      options.enableAutoPerformanceTracing = true;
+    },
+    appRunner: () => runApp(const App()),
+  );
+
   // runApp(DevicePreview(enabled: !kReleaseMode, builder: (context) => const App()));
+  // runApp(const App());
 }
 
 class App extends StatelessWidget {
@@ -71,12 +92,11 @@ class App extends StatelessWidget {
         home: const ScreensWrapper(),
         // locale: DevicePreview.locale(context),
         // builder: DevicePreview.appBuilder,
+        navigatorObservers: [SentryNavigatorObserver()],
       ),
     );
   }
 }
-
-
 
 // TODO sign
 // TODO about

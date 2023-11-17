c0302b07d93a4bc00990ceb9ea9c4e589dd19ea2
digiborimusik <digiborimusik@gmail.com>
2023-11-17 17:40:20 +0200

    sampling service: remoce deprecated code

--- lib/data/services/stats_sampling_service.dart
+++ lib/data/services/stats_sampling_service.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first, unused_field, avoid_print
 import 'dart:async';
-import 'dart:collection';
-import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:xdslmt/core/app_logger.dart';
 import 'package:xdslmt/data/models/app_settings.dart';
 import 'package:xdslmt/data/models/line_stats.dart';
@@ -11,25 +10,15 @@ import 'package:xdslmt/data/repositories/current_sampling_repo.dart';
 import 'package:xdslmt/data/repositories/stats_repo.dart';
 import 'package:xdslmt/data/repositories/settings_repo.dart';
 
-class StatsSamplingService extends ChangeNotifier {
+class StatsSamplingService {
   final SettingsRepository _settingsRepository;
   final StatsRepository _statsRepository;
   final CurrentSamplingRepository _currentSamplingRepository;
   StatsSamplingService(this._statsRepository, this._settingsRepository, this._currentSamplingRepository);
 
-  @Deprecated('move to shared repo')
-  SnapshotStats? _snapshotStats;
-  @Deprecated('move to shared repo')
-  SnapshotStats? get snapshotStats => _snapshotStats;
-
-  @Deprecated('move to shared repo')
-  final _samplesQueue = Queue<LineStats>();
-  @Deprecated('move to shared repo')
-  Iterable<LineStats> get lastSamples => _samplesQueue;
-
-  StreamSubscription? samplingSub;
-  @Deprecated('move to shared repo')
-  bool get samplingActive => samplingSub != null;
+  final _mc = const MethodChannel('main');
+  StreamSubscription? _samplingSub;
+  bool get samplingActive => _samplingSub != null;
 
   Stream<(LineStats lineStats, SnapshotStats snapshotStats)> createLineStatsStream() async* {
     AppSettings settings = await _settingsRepository.getSettings;
@@ -38,6 +27,9 @@ class StatsSamplingService extends ChangeNotifier {
 
     NetUnitClient client = NetUnitClient.fromSettings(settings, snapshotId);
     SnapshotStats snapshotStats = SnapshotStats.create(snapshotId, settings.host, settings.login, settings.pwd);
+
+    if (settings.wakeLock) _mc.invokeMethod('startWakeLock').ignore();
+    if (settings.foregroundService) _mc.invokeMethod('startForegroundService').ignore();
 
     AppLogger.info(name: 'StatsSamplingService', 'Run sampling: $snapshotId');
     AppLogger.debug(name: 'StatsSamplingService', '$settings');
@@ -70,48 +62,30 @@ class StatsSamplingService extends ChangeNotifier {
     }
   }
 
-  // Start Network Unit stats sampling using the current settings
   runSampling() async {
-    if (samplingSub != null) return;
+    if (samplingActive) return;
 
     AppLogger.debug(name: 'StatsSamplingService', 'Run sampling');
 
-    _snapshotStats = null;
-    _samplesQueue.clear();
-
-    samplingSub = createLineStatsStream().listen(
-      (event) {
-        final (lineStats, snapshotStats) = event;
-        _addLineStatsToQueue(lineStats);
-        _snapshotStats = snapshotStats;
-        notifyListeners();
-        _currentSamplingRepository.updateStats(lineStats, snapshotStats);
-      },
+    _samplingSub = createLineStatsStream().listen(
+      (event) => _currentSamplingRepository.updateStats(event.$1, event.$2),
       onDone: () => stopSampling(), // Actually, this should never happen
     );
-
-    notifyListeners();
+    _currentSamplingRepository.wipeStats();
     _currentSamplingRepository.updateStatus(true);
   }
 
   stopSampling({bool wipeQueue = true}) {
+    if (!samplingActive) return;
+
     AppLogger.info(name: 'StatsSamplingService', 'Stop sampling');
 
-    samplingSub?.cancel();
-    samplingSub = null;
+    _mc.invokeMethod('stopForegroundService');
+    _mc.invokeMethod('stopWakeLock');
 
-    notifyListeners();
+    _samplingSub?.cancel();
+    _samplingSub = null;
+
     _currentSamplingRepository.updateStatus(false);
-  }
-
-  _addLineStatsToQueue(LineStats lineStats) {
-    _samplesQueue.addLast(lineStats);
-    if (_samplesQueue.length > 500) _samplesQueue.removeFirst();
-  }
-
-  @override
-  void dispose() {
-    stopSampling();
-    super.dispose();
   }
 }
